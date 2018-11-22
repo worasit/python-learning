5eb8503c5a6d1f57bb517784d045336828411090
worasit daimongkol <worasit.dmk501@gmail.com>
2018-11-22 17:16:07 +0700

    Initial package for the chapter05 Decorators

---
+++ mastering/decorators/__init__.py
@@ -0,0 +1,11 @@
+"""
+Decorators are essentially function/class wrappers that can be used to modify the input,
+output, or even the function/class itself before executing it.
+
+The following are the topics covered:
+ - Decorating functions
+ - Decorating class functions
+ - Decorating classes
+ - Using classes as decorators
+ - Useful decorator in the Python standard library
+"""
